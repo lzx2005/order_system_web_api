da927571a87264947aba83dc0076f797cb89561a
lzx2005 <978744976@qq.com>
2017-04-19 11:36:06 +0800

    完善打包代码

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -26,7 +26,7 @@ CREATE TABLE `dish` (
   `name` varchar(255) NOT NULL COMMENT '菜品名称',
   `price` decimal(7,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '单价',
   `belong` int(11) unsigned NOT NULL,
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime COMMENT '创建时间',
   `logo` bigint(20) NOT NULL COMMENT '封面图片',
   `type` bigint(20) unsigned NOT NULL COMMENT '菜品类型',
   `image` bigint(20) DEFAULT NULL,
